30b1788df8f10e7544c91953d1a2f896de86a5e1
GitMasterBart <bartengels15@live.nl>
2022-07-26 09:45:40 +0200

    pep8 scripts/connect_to_database.py

--- scripts/connect_to_database.py
+++ scripts/connect_to_database.py
@@ -1,23 +1,37 @@
 #!/env/bin/python3
-import sys
+
+"""
+This class pushes the result data in a database, it uses mysql.connector
+to make contact with the mysql database. It is a class that needs three variables:
+ file, r_id(research_id), u_id (user_id)"""
+
+__author__ = "Bart Engels"
+__date__ = "28-07-2022"
+__version__ = "v1"
+
 import mysql.connector
 from mysql.connector import Error
 import pandas as pd
 import pandas.errors
 
+
 class DbConnector:
+    """
+    Parsers a table and writes it to the database
+    that is given in mysql.connector.connect
+    add informtion yourself: host, database, user, password
+    """
     def __init__(self, file, r_id, u_id):
         self.file = file
         self.r_id = r_id
         self.u_id = u_id
 
     try:
-
         def write_to_dump_table(self):
-            connection = mysql.connector.connect(host='localhost',
-                                                      database='biobakery',
-                                                      user='root',
-                                                      password='rakker444')
+            connection = mysql.connector.connect(host='',
+                                                      database='',
+                                                      user='',
+                                                      password='')
             cursor = connection.cursor()
             cursor.execute("show tables;")
             myresult = cursor.fetchall()
@@ -25,8 +39,8 @@ class DbConnector:
             transformed_table = pd.DataFrame(pd.melt(pd.read_table(
                 self.file,
                 lineterminator='\n'), id_vars=first_column_object))
-            print(transformed_table)
-            sql = "INSERT INTO biobakery_dumptable (gene, family, sample, result, researches_id_id, user_id_id) VALUES (%s,%s,%s,%s,%s,%s) "
+            sql = "INSERT INTO biobakery_dumptable (gene, family, sample, " \
+                  "result, researches_id_id, user_id_id) VALUES (%s,%s,%s,%s,%s,%s) "
             family = ""
             compleet_list = []
 
@@ -45,25 +59,9 @@ class DbConnector:
                         # print([gene,family])
                         sample = str(transformed_table.values[i][1])
                         result = transformed_table.values[i][2]
-                # print((gene, family, sample, result, self.r_id, self.u_id))
-
-
                 compleet_list.append((gene,family,sample,result, self.r_id, self.u_id))
 
             cursor.executemany(sql,compleet_list)
             connection.commit()
-            # print(range(len(transformed_table)))
-
-
-
-    except (Error, pandas.errors.Rule) :
+    except (Error, pandas.errors.Rule):
         print("Error while connecting to MySQL")
-
-def main():
-    DbConnector("/Users/bengels/sample_results/demo_pathcoverage.tsv", 46, 1).write_to_dump_table()
-
-
-
-if __name__ == '__main__':
-    main()
-
